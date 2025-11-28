dedcb541d972121258b6c9329b563461a454ef6e
hishamrehab <hishamrehab30@gmail.com>
2025-11-28 23:15:44 +0200

    Loading Fallback

--- 3 - Data Fetching/data-fetching/app/(content)/news/page.js
+++ 3 - Data Fetching/data-fetching/app/(content)/news/page.js
@@ -1,10 +1,10 @@
 import NewsList from '@/components/news-list';
 import { getAllNews } from '@/lib/news';
 
-export default  async function NewsPage() {
-    const news = getAllNews();
+export default async function NewsPage() {
+    const news = await getAllNews();
     
-
+      
   return (
     <>
       <h1>News Page</h1>
