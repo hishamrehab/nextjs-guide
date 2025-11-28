568a07b769be987cd222c586dd80a33eac2fb17c
hishamrehab <hishamrehab30@gmail.com>
2025-11-28 15:10:54 +0200

    Nestef Routes inside dynamic routes

---
+++ 2 - Routing & Page Rendering/components/main-header.js
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import NavLink from './nav-link';
+
+export default function MainHeader() {
+    return (
+    <header id="main-header">
+      <div id="logo">
+        <Link href="/">NextNews</Link>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/news" className={
+              path.startsWith("/news")
+               ? "active" : undefined}>News</Link>
+          </li>
+
+            <li>
+          <NavLink href="/news">
+             News
+          </NavLink>
+          </li>
+
+          
+            <li>
+          <NavLink href="/archive">
+            Archive
+          </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
---
+++ 2 - Routing & Page Rendering/components/nav-link.js
@@ -0,0 +1,20 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function NavLink({href , children}) {
+    const path = usePathname();
+    
+
+
+    return (
+            <Link
+             className={
+              path.startsWith(href)
+               ? "active" : undefined}
+              href={href}>
+             {children}
+            </Link>
+    )
+}
