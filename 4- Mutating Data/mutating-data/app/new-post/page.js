fdde1511a80bd9f694705ce10c200437cd419522
hishamrehab <hishamrehab30@gmail.com>
2025-12-07 11:50:41 +0200

    proving a user feedback with the useFormStatus Hook

--- 4- Mutating Data/mutating-data/app/new-post/page.js
+++ 4- Mutating Data/mutating-data/app/new-post/page.js
@@ -1,12 +1,25 @@
+import FormSubmit from "@/components/form-submit";
+import { storePost } from "@/lib/posts";
+import {redirect} from "next/navigation"
+
+
+
 export default function NewPostPage() {
   async function createPost(formData) {
    "use server";
   const title =  formData.get("title");
-  const image = formData.get("image");
   const content = formData.get("content");
+  const image = formData.get("image");
 
-  console.log(title, image, content);
-}
+ await storePost({
+    imageUrl: "", 
+     title,
+     content,
+     userId : 1
+    });
+ 
+    redirect("/feed");
+  }
 
   return (
     <>
@@ -30,8 +43,7 @@ export default function NewPostPage() {
           <textarea id="content" name="content" rows="5" />
         </p>
         <p className="form-actions">
-          <button type="reset">Reset</button>
-          <button>Create Post</button>
+            <FormSubmit />
         </p>
       </form>
     </>
